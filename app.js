2d5082723d5d300a0b91474dee2f8f7ceb63bb47
Violette Abergel <violetteabergel@MacBook-Pro-de-Violette.local>
2025-10-23 10:54:14 +0200

    build and export output.ply

--- app.js
+++ app.js
@@ -29,32 +29,44 @@ async function main() {
     ]);
 
     // Appel de la fonction de traitement
-    process(vertices, faces);
+    const plyContent = buildPLY(vertices, faces);
+
+    // Écriture du fichier
+    fs.writeFileSync('./data/output.ply', plyContent, 'utf8');
+    console.log('Fichier output.ply généré avec succès !');
+
   } catch (error) {
     console.error('Erreur lors de la lecture des fichiers :', error);
   }
 }
 
 // Exemple de fonction de traitement
-function process(vertices, faces) {
+function buildPLY(vertices, faces) {
   console.log('Nombre de vertices :', vertices.length);
   console.log('Nombre de faces :', faces.length);
-  console.log('Premier vertex :')
-  console.log(vertices[0]);
-  console.log('Première face :', faces[0]);
-  // ... ton code ici ...
-
+  
   let header = `ply
   format ascii 1.0
-  element vertex ${vertices.length + 1}
+  element vertex ${vertices.length}
   property float x
   property float y
   property float z
-  element face ${faces.length + 1}
+  element face ${faces.length}
   property list uchar int vertex_indices
   end_header`;
 
   console.log(header);
+
+  // les vertex sont deja formattees correctement. Pour les faces on doit ajouter le nombre de vertices (3 pour un triangle)
+  const vertexLines = vertices.map(v => v.join(' '));
+  const faceLines = faces.map(f => {
+    // On anticipe le cas où les valeurs sont chargees comme des strings ["14","12","13"]
+    const indices = f.map(Number);
+    return `${indices.length} ${indices.join(' ')}`;
+  });
+
+  // on asssemble le tout
+  return [header, ...vertexLines, ...faceLines].join('\n');
 }
 
 main();
